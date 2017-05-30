c9425da3ea654e9f424aa446f32034211e27baf4
Tbone288 <tylerodom2@gmail.com>
2017-05-30 12:44:46 -0600

    added explosions

--- FlappyKirby/Guts/Controller/KirbyGame.cs
+++ FlappyKirby/Guts/Controller/KirbyGame.cs
@@ -3,6 +3,11 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Input.Touch;
 using FlappyKirby.Model;
+using System.Collections.Generic;
+using FlappyKirby.View;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Media;
+using System;
 
 namespace FlappyKirby.Controller
 {
@@ -28,6 +33,34 @@ namespace FlappyKirby.Controller
 		// A movement speed for the player
 		private float playerMoveSpeed;
 
+		// Image used to display the static background
+		private Texture2D mainBackground;
+
+		// Parallaxing Layers
+		private ParallaxingBackground bgLayer1;
+		private ParallaxingBackground bgLayer2;
+
+		// Enemies
+		private Texture2D enemyTexture;
+		private List<Enemy> enemies;
+
+		// The rate at which the enemies appear
+		private TimeSpan enemySpawnTime;
+		private TimeSpan previousSpawnTime;
+
+		// A random number generator
+		private Random random;
+
+		private Texture2D projectileTexture;
+		private List<Projectile> projectiles;
+
+		// The rate of fire of the player laser
+		private TimeSpan fireTime;
+		private TimeSpan previousFireTime;
+
+		private Texture2D explosionTexture;
+		private List<Animation> explosions;
+
 
 
 
@@ -39,7 +72,8 @@ namespace FlappyKirby.Controller
 
 		private void UpdatePlayer(GameTime gameTime)
 		{
-
+			
+			player.Update(gameTime);
 			// Get Thumbstick Controls
 			player.Position.X += currentGamePadState.ThumbSticks.Left.X * playerMoveSpeed;
 			player.Position.Y -= currentGamePadState.ThumbSticks.Left.Y * playerMoveSpeed;
@@ -65,6 +99,16 @@ namespace FlappyKirby.Controller
 			// Make sure that the player does not go out of bounds
 			player.Position.X = MathHelper.Clamp(player.Position.X, 0, GraphicsDevice.Viewport.Width - player.Width);
 			player.Position.Y = MathHelper.Clamp(player.Position.Y, 0, GraphicsDevice.Viewport.Height - player.Height);
+
+			// Fire only every interval we set as the fireTime
+			if (gameTime.TotalGameTime - previousFireTime > fireTime)
+			{
+				// Reset our current time
+				previousFireTime = gameTime.TotalGameTime;
+
+				// Add the projectile, but add it to the front and center of the player
+				AddProjectile(player.Position + new Vector2(player.Width / 2, 0));
+			}
 		}
 
 		/// <summary>
@@ -81,7 +125,30 @@ namespace FlappyKirby.Controller
 			player = new Player();
 
 			// Set a constant player move speed
-			playerMoveSpeed = 8.0f;
+			playerMoveSpeed = 6.0f;
+
+			bgLayer1 = new ParallaxingBackground();
+			bgLayer2 = new ParallaxingBackground();
+
+			// Initialize the enemies list
+			enemies = new List<Enemy>();
+
+			// Set the time keepers to zero
+			previousSpawnTime = TimeSpan.Zero;
+
+			// Used to determine how fast enemy respawns
+			enemySpawnTime = TimeSpan.FromSeconds(1.0f);
+
+			// Initialize our random number generator
+			random = new Random();
+
+			projectiles = new List<Projectile>();
+
+			// Set the laser to fire every quarter second
+			fireTime = TimeSpan.FromSeconds(.15f);
+
+			explosions = new List<Animation>();
+
 
 			base.Initialize();
 		}
@@ -95,10 +162,26 @@ namespace FlappyKirby.Controller
 			// Create a new SpriteBatch, which can be used to draw textures.
 			spriteBatch = new SpriteBatch(GraphicsDevice);
 
-			// Load the player resources 
-			Vector2 playerPosition = new Vector2(GraphicsDevice.Viewport.TitleSafeArea.X, GraphicsDevice.Viewport.TitleSafeArea.Y + GraphicsDevice.Viewport.TitleSafeArea.Height / 2);
+			// Load the player resources
+			Animation playerAnimation = new Animation();
+			Texture2D playerTexture = Content.Load<Texture2D>("Animation/KirbyAnimation");
+			playerAnimation.Initialize(playerTexture, Vector2.Zero, 128, 128, 2, 100, Color.White, 1f, true);
 
-			player.Initialize(Content.Load<Texture2D>("Texture/player"), playerPosition);
+			Vector2 playerPosition = new Vector2(GraphicsDevice.Viewport.TitleSafeArea.X, GraphicsDevice.Viewport.TitleSafeArea.Y + GraphicsDevice.Viewport.TitleSafeArea.Height / 2);
+			player.Initialize(playerAnimation, playerPosition);
+
+			// Load the parallaxing background
+			bgLayer1.Initialize(Content, "Texture/myBackground2", GraphicsDevice.Viewport.Width, -1);
+			bgLayer2.Initialize(Content, "Texture/myBackground2", GraphicsDevice.Viewport.Width, -2);
+
+			mainBackground = Content.Load<Texture2D>("Texture/KirbyBackground");
+
+			enemyTexture = Content.Load<Texture2D>("Animation/Bronco Burt");
+
+			projectileTexture = Content.Load<Texture2D>("Texture/laser");
+
+			explosionTexture = Content.Load<Texture2D>("Animation/explosion");
+
 
 			//TODO: use this.Content to load your game content here 
 		}
@@ -128,6 +211,19 @@ namespace FlappyKirby.Controller
 			//Update the player
 			UpdatePlayer(gameTime);
 
+			// Update the parallaxing background
+			bgLayer1.Update();
+			bgLayer2.Update();
+
+			// Update the enemies
+			UpdateEnemies(gameTime);
+
+			// Update the collision
+			UpdateCollision();
+
+			// Update the projectiles
+			UpdateProjectiles();
+
 			// TODO: Add your update logic here
 
 			base.Update(gameTime);
@@ -145,12 +241,186 @@ namespace FlappyKirby.Controller
 
 			// Start drawing
 			spriteBatch.Begin();
+
+			spriteBatch.Draw(mainBackground, Vector2.Zero, Color.White);
+
+			// Draw the moving background
+			bgLayer1.Draw(spriteBatch);
+			bgLayer2.Draw(spriteBatch);
+
 			// Draw the Player 
 			player.Draw(spriteBatch);
+
+			// Draw the Enemies
+			for (int i = 0; i < enemies.Count; i++)
+			{
+				enemies[i].Draw(spriteBatch);
+			}
+
+			// Draw the Projectiles
+			for (int i = 0; i < projectiles.Count; i++)
+			{
+				projectiles[i].Draw(spriteBatch);
+			}
+
+			// Draw the explosions
+			for (int i = 0; i < explosions.Count; i++)
+			{
+				explosions[i].Draw(spriteBatch);
+			}
+
+			// Update the explosions
+			UpdateExplosions(gameTime);
+
 			// Stop drawing 
 			spriteBatch.End();
-
 			base.Draw(gameTime);
 		}
+
+		private void AddEnemy()
+		{
+			// Create the animation object
+			Animation enemyAnimation = new Animation();
+
+			// Initialize the animation with the correct animation information
+			enemyAnimation.Initialize(enemyTexture, Vector2.Zero, 100, 100, 2, 100, Color.White, 1f, true);
+
+			// Randomly generate the position of the enemy
+			Vector2 position = new Vector2(GraphicsDevice.Viewport.Width + enemyTexture.Width / 2, random.Next(100, GraphicsDevice.Viewport.Height - 100));
+
+			// Create an enemy
+			Enemy enemy = new Enemy();
+
+			// Initialize the enemy
+			enemy.Initialize(enemyAnimation, position);
+
+			// Add the enemy to the active enemies list
+			enemies.Add(enemy);
+		}
+
+		private void UpdateEnemies(GameTime gameTime)
+		{
+			// Spawn a new enemy enemy every 1.5 seconds
+			if (gameTime.TotalGameTime - previousSpawnTime > enemySpawnTime)
+			{
+				previousSpawnTime = gameTime.TotalGameTime;
+
+				// Add an Enemy
+				AddEnemy();
+			}
+
+			// Update the Enemies
+			for (int i = enemies.Count - 1; i >= 0; i--)
+			{
+				enemies[i].Update(gameTime);
+
+				if (enemies[i].Active == false)
+				{
+					// If not active and health <= 0
+					if (enemies[i].Health <= 0)
+					{
+						// Add an explosion
+						AddExplosion(enemies[i].Position);
+					}
+					enemies.RemoveAt(i);
+				}
+			}
+		}
+
+		private void UpdateCollision()
+		{
+			// Use the Rectangle's built-in intersect function to 
+			// determine if two objects are overlapping
+			Rectangle rectangle1;
+			Rectangle rectangle2;
+
+			// Only create the rectangle once for the player
+			rectangle1 = new Rectangle((int)player.Position.X, (int)player.Position.Y, player.Width, player.Height);
+
+			// Do the collision between the player and the enemies
+			for (int i = 0; i < enemies.Count; i++)
+			{
+				rectangle2 = new Rectangle((int)enemies[i].Position.X, (int)enemies[i].Position.Y, enemies[i].Width, enemies[i].Height);
+
+				// Determine if the two objects collided with each other
+				if (rectangle1.Intersects(rectangle2))
+				{
+					// Subtract the health from the player based on
+					// the enemy damage
+					player.Health -= enemies[i].Damage;
+
+					// Since the enemy collided with the player
+					// destroy it
+					enemies[i].Health = 0;
+
+					// If the player health is less than zero we died
+					if (player.Health <= 0)
+					{
+						player.Active = false;
+					}
+				}
+			}
+			// Projectile vs Enemy Collision
+			for (int i = 0; i < projectiles.Count; i++)
+			{
+				for (int j = 0; j < enemies.Count; j++)
+				{
+        // Create the rectangles we need to determine if we collided with each other
+        rectangle1 = new Rectangle((int)projectiles[i].position.X - projectiles[i].Width / 2, (int) projectiles[i].position.Y - 
+ projectiles[i].Height / 2, projectiles[i].Width, projectiles[i].Height);
+
+        rectangle2 = new Rectangle((int)enemies[j].Position.X - enemies[j].Width / 2, (int) enemies[j].Position.Y - enemies[j].Height / 2, enemies[j].Width, enemies[j].Height);
+
+        // Determine if the two objects collided with each other
+        if (rectangle1.Intersects(rectangle2))
+        {
+            enemies[j].Health -= projectiles[i].Damage;
+            projectiles[i].Active = false;
+        }
+    }
+}
+		}
+
+		private void AddProjectile(Vector2 position)
+		{
+			Projectile projectile = new Projectile();
+			projectile.Initialize(GraphicsDevice.Viewport, projectileTexture, position);
+			projectiles.Add(projectile);
+		}
+
+		private void UpdateProjectiles()
+		{
+			// Update the Projectiles
+			for (int i = projectiles.Count - 1; i >= 0; i--)
+			{
+				projectiles[i].Update();
+
+				if (projectiles[i].Active == false)
+				{
+					projectiles.RemoveAt(i);
+				}
+			}
+		}
+
+		private void AddExplosion(Vector2 position)
+		{
+			Animation explosion = new Animation();
+			explosion.Initialize(explosionTexture, position, 134, 134, 12, 45, Color.White, 1f, false);
+			explosions.Add(explosion);
+		}
+
+		private void UpdateExplosions(GameTime gameTime)
+		{
+			for (int i = explosions.Count - 1; i >= 0; i--)
+			{
+				explosions[i].Update(gameTime);
+				if (explosions[i].Active == false)
+				{
+					explosions.RemoveAt(i);
+				}
+			}
+		}
+
+
 	}
 }
--- FlappyKirby/Guts/Model/Player.cs
+++ FlappyKirby/Guts/Model/Player.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-
+using FlappyKirby.View;
+using FlappyKirby.Controller;
 
 namespace FlappyKirby.Model
 {
@@ -10,11 +11,11 @@ namespace FlappyKirby.Model
 	{
 
 		// Animation representing the player
-		private Texture2D playerTexture;
-		public Texture2D PlayerTexture
+		private Animation playerAnimation;
+		public Animation PlayerAnimation
 		{
-			get { return playerTexture; }
-			set { playerTexture = value; }
+			get { return playerAnimation; }
+			set { playerAnimation = value; }
 		}
 
 		// Position of the Player relative to the upper left side of the screen
@@ -40,39 +41,43 @@ namespace FlappyKirby.Model
 		// Get the width of the player ship
 		public int Width
 		{
-			get { return PlayerTexture.Width; }
+			get { return playerAnimation.FrameWidth; }
 		}
 
 		// Get the height of the player ship
 		public int Height
 		{
-			get { return PlayerTexture.Height; }
+			get { return playerAnimation.FrameHeight; }
 		}
 
 
-		public void Initialize(Texture2D texture, Vector2 position)
+		// Initialize the player
+		public void Initialize(Animation animation, Vector2 position)
 		{
-			PlayerTexture = texture;
+			playerAnimation = animation;
 
-			// Set the starting position of the player around the middle of the screen and to the back 
+			// Set the starting position of the player around the middle of the screen and to the back
 			Position = position;
 
-			// Set the player to be active 
+			// Set the player to be active
 			Active = true;
 
 			// Set the player health
 			Health = 100;
 		}
-
-		public void Update()
+		// Update the player animation
+		public void Update(GameTime gameTime)
 		{
+			playerAnimation.Position = Position;
+			playerAnimation.Update(gameTime);
 		}
 
+
+		// Draw the player
 		public void Draw(SpriteBatch spriteBatch)
 		{
-			spriteBatch.Draw(PlayerTexture, Position, null, Color.White, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0f);
+			playerAnimation.Draw(spriteBatch);
 		}
-
 
 	}
 }
